5a0232b8d265e7ea7ea6575ffcc9643bac6cbfd8
Hayden Juneau <66452827+ElephantInTheRom@users.noreply.github.com>
2021-10-04 16:11:04 -0400

    editing on game scene

--- Scripts/Scenes/Game.cs
+++ Scripts/Scenes/Game.cs
@@ -9,12 +9,30 @@ namespace Collapse.Scenes
         //Instances
         private PackedScene TowerScene { get; set; }
 
+        //Nodes
+        private Position2D SpawnPoint { get; set; }
+
         //Godot methods
         public override void _Ready()
         {
             base._Ready();
 
+            SpawnPoint = GetNode<Position2D>("Spawnpoint");
+
             TowerScene = GD.Load<PackedScene>("res://Scenes/Test/platforms.tscn");
+
+            //Spawn towers
+            Node2D t1 = TowerScene.Instance() as Node2D;
+            Node2D t2 = TowerScene.Instance() as Node2D;
+            Node2D t3 = TowerScene.Instance() as Node2D;
+
+            t1.Position = SpawnPoint.Position;
+            t2.Position = SpawnPoint.Position + new Vector2(100, 0);
+            t3.Position = SpawnPoint.Position + new Vector2(200, 0);
+
+            AddChild(t1);
+            AddChild(t2);
+            AddChild(t3);
         }
 
         public override void _Input(InputEvent @event)
@@ -30,7 +48,7 @@ namespace Collapse.Scenes
         {
             if(body is Player.Player)
             {
-
+                GetTree().ChangeScene("res://Scenes/MainTitle.tscn");
             }
         }
     }
